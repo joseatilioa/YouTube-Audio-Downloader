29c5766295961b5ca72664ec52c3e9ba19305e83
joseatilioa <97798977+joseatilioa@users.noreply.github.com>
2022-01-18 11:20:33 -0400

    Update YouTubeDownloader.py

--- YouTubeDownloader.py
+++ YouTubeDownloader.py
@@ -1,18 +1,12 @@
 from pytube import YouTube
 
 while True:
-    link = input('Introduce el link de YouTube: ')
-    try:
-        yt = YouTube(link)
-        yt = str
-    except:
-        print('Link no valido')
-        continue
-
+    link = input('Enter YouTube link: ')
+    yt = YouTube(link)
     print('Titulo: ', yt.title)
     yt.streams.filter(only_audio=True)
     ys = yt.streams.get_by_itag(251)
-    print('Descargando...')
-    ys.download('Desktop')
-    print('Descarga finalizada')
+    print('Downloading...')
+    ys.download('Music')
+    print('Download completed')
     continue
